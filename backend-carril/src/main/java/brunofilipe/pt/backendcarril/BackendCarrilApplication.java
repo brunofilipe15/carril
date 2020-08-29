cc196e2394a0e91ff0d551ed9b7d8be61d0a042d
Bruno <bruno_filip_15@hotmail.com>
2020-08-29 15:15:17 +0100

    init-heroku

---
+++ backend-carril/src/main/java/brunofilipe/pt/backendcarril/BackendCarrilApplication.java
@@ -0,0 +1,13 @@
+package brunofilipe.pt.backendcarril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCarrilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCarrilApplication.class, args);
+	}
+
+}
---
+++ backend-carril/src/test/java/brunofilipe/pt/backendcarril/BackendCarrilApplicationTests.java
@@ -0,0 +1,13 @@
+package brunofilipe.pt.backendcarril;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCarrilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
